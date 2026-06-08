4b04f596bb8936a7f424930dbcfae9139895befa
Annanya Gupta <gupta19annanya@gmail.com>
2026-06-08 20:27:34 +0530

    Implement basic calculator functionality

---
+++ basic calculator.py
@@ -0,0 +1,10 @@
+a = int (input("enter integer: "))
+b = int (input("enter integer: "))
+sum = a + b
+sub = a - b 
+mul = a * b
+div = a / b 
+print("the total is:" , sum)
+print("the subtraction is :" , sub)
+print("the multiplication is :" , mul)
+print("the division is :" , div)
