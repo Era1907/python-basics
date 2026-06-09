593589b550c8976bb019fe2fe1fee3ba8f3a5b4b
Annanya Gupta <gupta19annanya@gmail.com>
2026-06-09 20:07:31 +0530

    Create Profilecard.py

---
+++ Profilecard.py
@@ -0,0 +1,12 @@
+
+print("Kindly enter the following details to create your profile card: ")
+name = input("Name - ")
+age = int(input("Age - "))
+city = input("City - ")
+hobby = input("Hobby - ")
+
+print("\n----PROFILE CARD ----")
+print("Name - " , name)
+print("Age - " , age)
+print("City - " , city)
+print("Hobby - " , hobby)
